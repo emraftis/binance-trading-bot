58891f7fa5006c55d7a034cd139439777ef9d3c6
emraftis <82290450+emraftis@users.noreply.github.com>
2021-08-31 14:46:41 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,7 @@
 const ccxt = require("ccxt");
 require("dotenv").config();
 
-// Gets candle data from binance, to USDC.
-// to update - how can we get candle data for a certain period of time? Current candle is every 60,000.
-const getCandles = async function (symbol, timeframe) {
+const getCandleData = async function (symbol, timeframe) {
   const exchangeId = "binance",
     exchangeClass = ccxt[exchangeId],
     exchange = new exchangeClass({
@@ -13,16 +11,25 @@ const getCandles = async function (symbol, timeframe) {
       enableRateLimit: true,
     });
 
+  // Gets *real* candle data from binance (not testnet) for every trailing 24 hour period.
   const now = exchange.milliseconds();
   console.log(now);
-  const day = 60 * 60 * 24 * 1000; // milliseconds
+  const day = 60 * 60 * 24 * 1 * 1000; // milliseconds
   const since = now - day;
   const candleData = await exchange.fetch_ohlcv(
-    `${symbol}/USDC`,
+    `${symbol}/BUSD`,
     `${timeframe}`,
     since
   );
-  return console.log(candleData); // data returned: [ 1630412160000, 3428.62, 3431.55, 3422.79, 3424.5, 15.631 ] equals [timestamp(ms), openPrice, highPrice, lowPrice, closePrice, volume(inBaseCurrency) ]
+
+  //Turns the client to the testnet.
+  exchange.set_sandbox_mode(true);
+
+  // Gets current balance data from binanace API Account (TESTNET)
+  const balance = await exchange.fetchBalance();
+
+  // console log the data fetched.
+  return console.log({ candleData, balance }); // data returned: [ 1630412160000, 3428.62, 3431.55, 3422.79, 3424.5, 15.631 ] equals [timestamp(ms), openPrice, highPrice, lowPrice, closePrice, volume(inBaseCurrency) ]
 };
 
-getCandles("ETH", "15m");
+getCandleData("ETH", "15m");
