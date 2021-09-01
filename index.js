e1f5227dd26d0ede78236cf022bb8ad2ef775936
emraftis <82290450+emraftis@users.noreply.github.com>
2021-09-01 10:33:15 -0600

    average value logic
    added logic to calculate a 3-day average price based on 15min price data (high+low/2)

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 const ccxt = require("ccxt");
 require("dotenv").config();
 
-const getCandleData = async function (symbol, timeframe) {
+const getData = async function (symbol, timeframe) {
+  // Establishes the client connection to Binance (real binance network)
+  // Because price data on Binance's sandbox is not real-time, we FIRST use real-time price data from the real binance network to test
+  // AND THEN after we get price data, we turn to the testnet for account related functions.
   const exchangeId = "binance",
     exchangeClass = ccxt[exchangeId],
     exchange = new exchangeClass({
@@ -11,20 +14,81 @@ const getCandleData = async function (symbol, timeframe) {
       enableRateLimit: true,
     });
 
-  // Gets *real* candle data from binance (not testnet) for every trailing 24 hour period.
+  // Gets 15m candle data from binance mainnet for a trailing 3 day period.
   const now = exchange.milliseconds();
-  console.log(now);
-  const day = 60 * 60 * 24 * 1 * 1000; // milliseconds
-  const since = now - day;
+  const lengthOfTime = 60 * 60 * 24 * 3 * 1000;
+  const since = now - lengthOfTime;
   const candleData = await exchange.fetch_ohlcv(
     `${symbol}/BUSD`,
     `${timeframe}`,
     since
-  );
+  ); // This returns an array called candleData[] with each array item being an array of [OHLCV values].
 
-  //Turns the client to the testnet.
+  //
+  //
+  //
+  //
+  //
+  //
+  // Trading logic... checking every ~15/16mins.
+  //
+  //
+  //
+  //
+  //
+
+  //Establish a CURRENT AVERAGE PRICE based on 3-day, 1hr closing price data.
+  const averagePrices = [];
+  for (let i = 0; i < candleData.length; i++) {
+    averagePrices.push((candleData[i][2] + candleData[i][3]) / 2);
+  }
+  const currentAvgPrice =
+    averagePrices.reduce((acc, value) => acc + value) / averagePrices.length;
+
+  // After +/-3% change from CURRENT AVERAGE PRICE, set either buySignal, sellSignal, or holdSignal {
+
+  //  1.  Reset CURRENT AVERAGE PRICE to a new price based on new trailing 3-day data.
+
+  //  2. Determine if its time to buy, sell, or hold.
+
+  //    if holdSignal && last 3 x 15min candles are red? {
+  //        sell 10% of ETH.
+  //        then change/keep to sellSignal.
+  //    }
+  //    if holdSignal && last 3 x 15min candles are green? {
+  //        hold.
+  //    }
+  //    if sellSignal && last 3 x 15min candles are green? {
+  //        buy $100 worth of ETH
+  //        set to holdSignal.
+  //    }
+  //    if sellSignal && last 3 x 15min candles are red? {
+  //        sell signal.
+  //    }
+  //    if buySignal && last 3 x 15min candles are green? {
+  //        set holdSignal
+  //    } else {
+  //        set buy or sell signal.
+  //    }
+  //
+  //
+
+  // Example: Assume ETH/BUSD and we hold $1,000 worth of BUSD and ETH 50/50.
+  //        Every 'big' a.k.a 3%? movement in price, we buy (or sell) 10%? 5%? of original holdings (of amount invested). i.e. if buy, we buy $100 worth. If sell, we only sell 10% of ETH holdings.
+  //        Establish a base price for ETH @ T=0.
+  //        keep checking every 15mins OR until the price of ETH increases or decreases by 3% from the original price, then set a new buy/hold/sell signal.
+
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //*****Turns the client to the testnet.*****
   exchange.set_sandbox_mode(true);
-
   // Gets current balance data from binanace API Account (TESTNET)
   const balance = await exchange.fetchBalance();
 
@@ -32,4 +96,4 @@ const getCandleData = async function (symbol, timeframe) {
   return console.log({ candleData, balance }); // data returned: [ 1630412160000, 3428.62, 3431.55, 3422.79, 3424.5, 15.631 ] equals [timestamp(ms), openPrice, highPrice, lowPrice, closePrice, volume(inBaseCurrency) ]
 };
 
-getCandleData("ETH", "15m");
+getData("ETH", "15m");
